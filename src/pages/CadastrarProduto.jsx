7f2b2ef7739d65013eae9b116921a47540243ad5
Gabriel Loureiro <loureirogabriel672@gmail.com>
2024-12-02 14:24:35 -0300

    pronto

--- src/pages/CadastrarProduto.jsx
+++ src/pages/CadastrarProduto.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import './ComponenteCadastrarProduto.css'
 
 const CadastrarProduto = () => {
   return (
-    <div>
-      asd
-    </div>
+    <div className="sas" style={{
+      marginTop: '259px',
+  }}>
+    <h1>Cadastrar Produto</h1>
+    <form
+      action=""
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",  
+        alignItems: "center",
+      }}
+    >
+      <input type="text" placeholder="Nome" />
+      <input type="text" placeholder="Preço" />
+      <input type="text" placeholder="Categoria" />
+      <a className="botao" href="./" style={{
+          margin: '20px',
+          padding: '20px 155px',
+          borderRadius: '5px',
+          textDecoration: 'none',
+          background: 'red',
+          color: 'white',
+      }}>Cadastrar Produto</a>
+    </form> 
+  </div>
   )
 }
 
